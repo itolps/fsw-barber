b48d13d530c63ae94fff5fccac2701f4098c03fa
Italo Lopes <italobragalopes19@gmail.com>
2024-08-10 23:54:13 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -28,6 +31,30 @@ const Home = () => {
             fill
             className="rounded-xl object-cover"
           />
+        </div>
+
+        <div>
+          <Card className="mt-6">
+            <CardContent className="flex justify-between p-0">
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge className="w-fit">Confirmado</Badge>
+                <h3 className="font-semibold">Corte de Cabelo</h3>
+
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://utfs.io/f/0522fdaf-0357-4213-8f52-1d83c3dcb6cd-18e.png"></AvatarImage>
+                  </Avatar>
+                  <p className="text-sm">Barbearia do Italo</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+                <p className="text-sm">Agosto</p>
+                <p className="text-2xl">05</p>
+                <p className="text-sm">20:00</p>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
